ec182635d3b8ffdcbfccfad93ed3d18a8e9fed87
AaronHsieh <piafyoyo06@gmail.com>
2024-10-11 21:12:57 +0800

    feat: 新增 @nuxt/fonts 套件

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,15 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: '2024-04-03',
-  devtools: { enabled: true }
-})
+  compatibilityDate: "2024-04-03",
+  devtools: { enabled: true },
+  modules: ["@nuxtjs/google-fonts", "@nuxt/fonts"],
+  googleFonts: {
+    families: {
+      "Noto+Sans+TC": [100, 300, 400, 500, 700, 900],
+    },
+    download: true,
+    inject: true,
+    display: "swap",
+  },
+  css: ["~/assets/css/fonts.css"],
+});
