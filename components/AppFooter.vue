3eefcbfa39206d69e754d7a1f1d0493e4b36f54a
AaronHsieh <piafyoyo06@gmail.com>
2024-10-13 16:08:40 +0800

    新增網站 Footer

---
+++ components/AppFooter.vue
@@ -0,0 +1,15 @@
+<template>
+  <footer>
+    <p>Fake Store API by 謝宗佑</p>
+  </footer>
+</template>
+
+<style scoped>
+footer {
+  background-color: #333;
+  color: #f5f5f5;
+  text-align: center;
+  padding: 16px 0;
+  margin-top: 48px;
+}
+</style>
