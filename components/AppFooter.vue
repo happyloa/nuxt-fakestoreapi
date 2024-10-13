d7ad9e028c2ea391d1d3dbc4529e9893786bdc01
AaronHsieh <piafyoyo06@gmail.com>
2024-10-13 16:15:59 +0800

    新增 repo 連結設定

--- components/AppFooter.vue
+++ components/AppFooter.vue
@@ -1,6 +1,9 @@
 <template>
   <footer>
     <p>Fake Store API by 謝宗佑</p>
+    <a href="https://github.com/happyloa/nuxt-fakestoreapi" target="_blank"
+      >GitHub Repo 連結</a
+    >
   </footer>
 </template>
 
@@ -9,7 +12,15 @@ footer {
   background-color: #333;
   color: #f5f5f5;
   text-align: center;
-  padding: 16px 0;
+  padding: 20px 0;
   margin-top: 48px;
+}
+
+footer p {
+  margin-bottom: 8px;
+}
+
+footer a {
+  color: inherit;
 }
 </style>
