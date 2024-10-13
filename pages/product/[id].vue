0b3bde1e7214dc97a60578b2b9c0f8251e570365
AaronHsieh <piafyoyo06@gmail.com>
2024-10-13 12:45:38 +0800

    依照目前首頁的進度來修改商品單獨頁面

--- pages/product/[id].vue
+++ pages/product/[id].vue
@@ -1,38 +1,58 @@
 <script setup>
-import { ref, onMounted } from "vue";
 import { useRoute } from "vue-router";
+import { useFetch } from "#app";
 
-const product = ref({});
 const route = useRoute();
+const { data: product } = useFetch(
+  () => `https://fakestoreapi.com/products/${route.params.id}`
+);
 
-onMounted(async () => {
-  const res = await fetch(
-    `https://fakestoreapi.com/products/${route.params.id}`
-  );
-  product.value = await res.json();
-
-  useSeoMeta({
-    title: `${product.value.title} | Fake Store API 商品資料串接練習`,
-    ogTitle: `${product.value.title} | Fake Store API 商品資料串接練習`,
-    description: "Fake Store API 商品資料串接練習 by Aaron",
-    ogDescription: "Fake Store API 商品資料串接練習 by Aaron",
-  });
+useSeoMeta({
+  title: `${product?.value?.title ?? ""} | Fake Store API 商品資料串接練習`,
+  ogTitle: `${product?.value?.title ?? ""} | Fake Store API 商品資料串接練習`,
+  description: "Fake Store API 商品資料串接練習 by Aaron",
+  ogDescription: "Fake Store API 商品資料串接練習 by Aaron",
 });
 </script>
 
 <template>
-  <div>
-    <h1>{{ product.title }}</h1>
-    <img :src="product.image" alt="product image" class="product-image" />
-    <p>{{ product.description }}</p>
-    <p><strong>價格：</strong> {{ product.price | currency }}</p>
-  </div>
+  <main class="container">
+    <section v-if="product" class="product-details">
+      <h1>{{ product.title }}</h1>
+      <img :src="product.image" alt="product image" class="product-image" />
+      <p>{{ product.description }}</p>
+      <p><strong>價格：</strong> ${{ product.price | currency }}</p>
+    </section>
+  </main>
 </template>
 
 <style scoped>
+.container {
+  max-width: 1200px;
+  margin: 0 auto;
+  padding: 0 20px;
+}
+
+.product-details {
+  text-align: center;
+  margin: 48px 0;
+}
+
+.product-details h1 {
+  font-size: 36px;
+  font-weight: 700;
+  margin-bottom: 24px;
+}
+
+.product-details p {
+  font-size: 20px;
+  margin-bottom: 16px;
+}
+
 .product-image {
   width: 300px;
   height: 300px;
   object-fit: cover;
+  margin-bottom: 24px;
 }
 </style>
