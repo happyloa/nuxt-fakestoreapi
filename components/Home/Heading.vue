9324d9fa0c3295e0894f280a28697fe35c05cb7c
AaronHsieh <piafyoyo06@gmail.com>
2024-10-14 08:42:59 +0800

    feat: 新增首頁標題元件的內容

--- components/Home/Heading.vue
+++ components/Home/Heading.vue
@@ -1,3 +1,40 @@
-<template></template>
+<template>
+  <hgroup class="heading-group">
+    <h1>Fake Store API 商品資料串接練習</h1>
+    <p>
+      參考資料：<a href="https://fakestoreapi.com/" target="_blank"
+        >Fake Store API</a
+      >
+    </p>
+  </hgroup>
+</template>
 
-<style scoped></style>
+<style scoped>
+.heading-group {
+  text-align: center;
+  margin: 48px 0;
+}
+
+.heading-group h1 {
+  font-size: 42px;
+  font-weight: 700;
+  line-height: 1.2;
+  margin-bottom: 16px;
+}
+
+.heading-group p {
+  font-size: 18px;
+}
+
+@media (max-width: 1024px) {
+  .heading-group h1 {
+    font-size: 36px;
+  }
+}
+
+@media (max-width: 576px) {
+  .heading-group h1 {
+    font-size: 28px;
+  }
+}
+</style>
