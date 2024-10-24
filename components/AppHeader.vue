c83c9f5ce20972000987d4f8c6536abd23249571
AaronHsieh <piafyoyo06@gmail.com>
2024-10-24 20:10:25 +0800

    feat: 新增 header

---
+++ components/AppHeader.vue
@@ -0,0 +1,35 @@
+<template>
+  <header>
+    <nav>
+      <NuxtLink to="/">Fake Store API</NuxtLink>
+    </nav>
+  </header>
+</template>
+
+<style scoped>
+/* 頁尾的樣式設置 */
+header {
+  background-color: #e0e5e9;
+  color: #262626;
+}
+
+header nav {
+  max-width: 1200px;
+  margin: 0 auto;
+  padding: 20px;
+}
+
+/* 連結的樣式，繼承文字顏色 */
+header a {
+  color: inherit;
+  font-size: 1.7rem;
+  font-weight: 700;
+  text-decoration: none;
+  transition: color 0.3s ease;
+}
+
+/* 當滑鼠懸停在連結上時，改變顏色 */
+header a:hover {
+  color: #0295db;
+}
+</style>
